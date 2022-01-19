f57dd27dea51b08c2d64c62079f949f552f98d5a
Viggy <jinshadow17@gmail.com>
2022-01-19 12:36:57 +0530

    Updates

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -11,7 +11,7 @@ export function errorHandler(
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
-  console.log("Something went wrong", err);
+  console.error(err);
 
   res.status(400).send({ errors: { message: "Something went wrong" } });
 }
