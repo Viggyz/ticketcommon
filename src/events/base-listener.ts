1e55d9bcb44856b1da82beaf6d212a276c0e3e29
Viggy <jinshadow17@gmail.com>
2022-01-23 14:06:20 +0530

    Updates

--- src/events/base-listener.ts
+++ src/events/base-listener.ts
@@ -35,7 +35,7 @@ export abstract class Listener<T extends Event> {
 
     subscription.on("message", (msg: Message) => {
       console.log(`Message recieved: ${this.subject} / ${this.queueGroupName}`);
-
+      console.log(msg);
       const parsedData = this.parseMessage(msg);
       this.onMessage(parsedData, msg);
     });
