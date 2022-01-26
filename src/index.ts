001b11f535dba4b3047d14c798b3a92e01ae57a5
Viggy <jinshadow17@gmail.com>
2022-01-26 18:33:23 +0530

    Updates

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,7 @@ export * from "./events/ticket-updated-event";
 export * from "./events/order-cancelled-event";
 export * from "./events/order-created-event";
 export * from "./events/expiration-complete-event";
+export * from "./events/payment-created";
 
 export * from "./events/types/order-status";
 export * from "./events/types/order-status";
